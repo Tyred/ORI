f39d6c94bdcb43984f64ae262f3d7b41f8ec8993
Yuri <49460014+Tyred@users.noreply.github.com>
2019-08-23 13:26:44 -0300

    Add files via upload
    Ta ai rapazeada

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <istream>
+#include <fstream>
+#include "interpreter.h"
+
+int main(int args, char** argv){
+	using namespace std;
+
+	string input;
+	interpreter a;
+	string data;
+	string aux;
+	char* text_data;
+	string str_text_data;
+	string* commands;
+	int i;
+
+	
+	// Modo Texto
+	if(argv[1] == NULL){
+		while(input != "EB" && getline(std::cin, input)){
+			toUpperString(input);
+			if(input.empty()) continue; // Ignora comando vazio
+			//erasespaces(input);
+			if(!a.parse(input)) break;
+		}
+	}
+	else{ // Modo Arquivo
+		// Abre e lê arquivo
+		
+		ifstream file;
+		file.open(argv[1]);
+		text_data = new char[get_file_size(&file)];
+		file.read(text_data, get_file_size(&file));
+		str_text_data = string(text_data);
+		
+
+		// Separa comando por comando
+		commands = new string[get_delimiters(str_text_data, '\n')];
+		for(i=0; i<get_delimiters(str_text_data, '\n'); i++){
+			commands[i] = split(str_text_data, '\n', i);
+			toUpperString(commands[i]);
+			if(!a.parse(commands[i])){
+				cout << "Erro detectado, na linha " << i+1 << endl;
+				return 1;
+			}
+		}
+		cout << "Fim dos dados!" << endl;
+	}
+}
